ca40128adeb8ab1f80048e75adc2ebe153580fc2
alexikiphenix <alexikiphenix@gmail.com>
2023-11-19 23:10:53 +0100

    App adding fx inputHandler v1.0 and submitHandler v1.0

--- App.js
+++ App.js
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import {     
   StyleSheet,
@@ -18,15 +18,31 @@ import {
 
 
 const App = () => {
+  const [product, setProduct] = useState('');
+  const [productsList, setProductsList] = useState([]);
+  const inputHandler = (value) =>
+  {
+    setProduct(value)
+  }
 
+  const submitHandler = () =>
+  {
+    console.log(product);
+  }
   return (
     <View style={styles.container}>
+        <Text>{ product }</Text>
         <View style={ styles.inputContainer }>
             <TextInput 
                 style={styles.textInput }
+                placeholder='nom produit'
+                onChangeText={ inputHandler }
+                value= { product }
             />
-            <Button
-                title="Ajouter"
+            <Button 
+              color='green'             
+              title="Ajouter"
+              onPress={ submitHandler }
             />
         </View>
     </View>
@@ -34,24 +50,23 @@ const App = () => {
 }
 
 const styles = StyleSheet.create(
-    {
-        container: {
-            padding: 40,
-            paddingTop: 60
-        },
-        inputContainer: {
-            flexDirection: "row",
-
-        },
-        textInput :{
-            borderColor: 'grey',
-            borderWidth: 1,
-            padding: 5,
-            paddingLeft: 5,
-            fontSize: 18,
-        }
-    }
+  {
+    container: {
+      padding: 38,
+      paddingTop: 55
+    },
+    inputContainer: {
+      flexDirection: "row",
+    },
+    textInput :{
+      flexGrow: 1,
+      borderColor: 'grey',
+      borderWidth: 1,
+      padding: 6,
+      paddingLeft: 8,
+      fontSize: 15,
+    },
+  }
 )
-
 
 export default App;
